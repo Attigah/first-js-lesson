ca82996e328344553b316b872f59995ea3ae4351
Attigah <Attigahbernard6@gmail.com>
2022-04-09 01:15:26 +0000

    Update firsthtml.js

--- firsthtml.js
+++ firsthtml.js
@@ -1,3 +1,13 @@
 let name = "Mosh";
-let age = 30;
+let age = 30.1;
 let isApproved = false;
+
+let person = {
+    name: 'Mosh',
+    age : 30
+};
+console.log(person);
+
+person.name = "John";
+
+console.log(person.name);
