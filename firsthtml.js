04ce88b6e01c40c69df60db5ded1741b883bcbe9
Attigah <Attigahbernard6@gmail.com>
2022-04-09 01:22:37 +0000

    keeping track, variables and array

--- firsthtml.js
+++ firsthtml.js
@@ -1,13 +1,3 @@
-let name = "Mosh";
-let age = 30.1;
-let isApproved = false;
 
-let person = {
-    name: 'Mosh',
-    age : 30
-};
-console.log(person);
-
-person.name = "John";
-
-console.log(person.name);
+let selectedColours = ['red' ,'blue'];
+console.log(selected.colours);
